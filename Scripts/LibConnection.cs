f3ed432ac65a8dde79f166d68306db5f1c6b8549
Phillip Hohenester <hohenester@ux3d.io>
2024-04-26 15:22:39 +0200

    added initial version of C# interface; added HeadTracking library

---
+++ Scripts/LibConnection.cs
@@ -0,0 +1,185 @@
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class LibConnection : MonoBehaviour
+{
+    //function definitions
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?initLibrary@G3D_UHTL@@YAHXZ", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int initLibrary();
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?deinitLibrary@G3D_UHTL@@YAHXZ", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int deinitLibrary();
+
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?setCalibrationPath@G3D_UHTL@@YAHPEBD@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int setCalibrationPath(in byte[] path);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?setConfigPath@G3D_UHTL@@YAHPEBD@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int setConfigPath(in byte[] path);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?setConfigFileName@G3D_UHTL@@YAHPEBD@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int setConfigFileName(in byte[] path);
+
+    // ------------------------------------------------
+    
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?registerHeadPositionChangedCallback@G3D_UHTL@@YAHPEAXP6AX_N1HHNNN0@Z@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int registerHeadPositionChangedCallback(out IntPtr listener, TNewHeadPositionCallback callback);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?unregisterHeadPositionChangedCallback@G3D_UHTL@@YAHPEAX@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int unregisterHeadPositionChangedCallback(IntPtr listener);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?registerShaderParametersChangedCallback@G3D_UHTL@@YAHPEAXP6AXPEBUCG3DShaderParameters@@0@Z@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int registerShaderParametersChangedCallback(IntPtr listener, TNewShaderParametersCallback callback);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?unregisterShaderParametersChangedCallback@G3D_UHTL@@YAHPEAX@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int unregisterShaderParametersChangedCallback(IntPtr listener);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?registerMessageCallback@G3D_UHTL@@YAHPEAXP6AXW4EMessageSeverity@@PEBD2220@Z@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int registerMessageCallback(IntPtr listener, TNewErrorMessageCallback callback);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?unregisterMessageCallback@G3D_UHTL@@YAHPEAX@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int unregisterMessageCallback(IntPtr listener);
+    
+    // ------------------------------------------------
+
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?useHimaxD2XXDevices@G3D_UHTL@@YAHXZ", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int useHimaxD2XXDevices();
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?usePmdFlexxDevices@G3D_UHTL@@YAHXZ", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int usePmdFlexxDevices();
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?initHeadTracking@G3D_UHTL@@YAHXZ", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int initHeadTracking();
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?deinitHeadTracking@G3D_UHTL@@YAHXZ", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int deinitHeadTracking();
+
+    // ------------------------------------------------
+
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?getHeadTrackingDeviceCount@G3D_UHTL@@YAHPEAH@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int getHeadTrackingDeviceCount(out int deviceCount);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?getHeadTrackingDeviceName@G3D_UHTL@@YAHHPEAPEAD@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int getHeadTrackingDeviceName(int deviceNumber, out IntPtr deviceName);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?getCurrentHeadTrackingDevice@G3D_UHTL@@YAHPEAPEAD@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int getCurrentHeadTrackingDevice(out IntPtr deviceName);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?setCurrentHeadTrackingDevice@G3D_UHTL@@YAHPEBD@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int setCurrentHeadTrackingDevice(in byte[] device);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?getHeadTrackingDeviceResolution@G3D_UHTL@@YAHPEAH0@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int getHeadTrackingDeviceResolution(out int horizontalResolution, out int verticalResolution);
+    
+    // ------------------------------------------------
+
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?getFirstValidCalibrationMatrixCol@G3D_UHTL@@YAHPEAH@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int getFirstValidCalibrationMatrixCol(out int value);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?getLastValidCalibrationMatrixCol@G3D_UHTL@@YAHPEAH@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int getLastValidCalibrationMatrixCol(out int value);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?getFirstValidCalibrationMatrixRow@G3D_UHTL@@YAHPEAH@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int getFirstValidCalibrationMatrixRow(out int value);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?getLastValidCalibrationMatrixRow@G3D_UHTL@@YAHPEAH@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int getLastValidCalibrationMatrixRow(out int value);
+    
+    // ------------------------------------------------
+
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?startHeadTracking@G3D_UHTL@@YAHXZ", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int startHeadTracking();
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?stopHeadTracking@G3D_UHTL@@YAHXZ", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int stopHeadTracking();
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?getHeadTrackingStatus@G3D_UHTL@@YAHPEA_N0@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int getHeadTrackingStatus([MarshalAs(UnmanagedType.U1)] out bool hasDevice, [MarshalAs(UnmanagedType.U1)] out bool isTracking);
+    
+    // ------------------------------------------------
+
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?getCurrentShaderParameters@G3D_UHTL@@YAHPEAUCG3DShaderParameters@@@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int getCurrentShaderParameters(out CG3DShaderParameters shaderParameters);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?setWindowPosition@G3D_UHTL@@YAHHH@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int setWindowPosition(int left, int top);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?setWindowSize@G3D_UHTL@@YAHHH@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int setWindowSize(int width, int height);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?setViewportOffset@G3D_UHTL@@YAHHH@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int setViewportOffset(int left, int top);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?setViewportSize@G3D_UHTL@@YAHHH@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int setViewportSize(int width, int height);
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?setScreenSize@G3D_UHTL@@YAHHH@Z", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int setScreenSize(int width, int height);
+    
+    // ------------------------------------------------
+
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?shiftViewToLeft@G3D_UHTL@@YAHXZ", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int shiftViewToLeft();
+    [DllImport("G3D_UniversalHeadTrackingLibrary.dll", EntryPoint="?shiftViewToRight@G3D_UHTL@@YAHXZ", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int shiftViewToRight();
+
+    enum EMessageSeverity
+    {
+        MS_INFO = 1,
+        MS_WARNING,
+        MS_ERROR,
+        MS_EXCEPTION
+    };
+
+    struct CG3DShaderParameters
+    {
+        // Viewport properties
+        int leftViewportPosition;   //< The left   position of the viewport in screen coordinates
+        int bottomViewportPosition; //< The bottom position of the viewport in screen coordinates
+
+        // Monitor properties
+        int screenWidth;  //< The screen width in pixels
+        int screenHeight; //< The screen height in pixels
+
+        int nativeViewCount;       // OLD: viewcount
+        int angleRatioNumerator;   // OLD: zwinkel
+        int angleRatioDenominator; // OLD: nwinkel
+        int leftLensOrientation;   // OLD: isleft
+        int BGRPixelLayout;        // OLD: isbgr
+
+        int mstart;         // TODO:   rename to viewOffset
+        int showTestFrame;  // OLD: test
+        int showTestStripe; // OLD: stest
+        int testGapWidth;   // OLD: testgap
+        int track;
+        int hqViewCount; // OLD: hqview
+        int hviews1;
+        int hviews2;
+        int blur;
+        int blackBorder; // OLD: bborder
+        int blackSpace;  // OLD: bspace
+        int bls;
+        int ble;
+        int brs;
+        int bre;
+
+        int zCorrectionValue;   // OLD: tvx
+        int zCompensationValue; // OLD: zkom
+    };
+
+    delegate void TNewErrorMessageCallback(
+        EMessageSeverity severity, in byte[] sender, in byte[] caption, in byte[] cause, in byte[] remedy, out IntPtr listener);
+
+    delegate void TNewHeadPositionCallback(
+        [MarshalAs(UnmanagedType.U1)] bool headDetected, [MarshalAs(UnmanagedType.U1)] bool imagePosIsValid, int imagePosX, int imagePosY,
+        double worldPosX, double worldPosY, double worldPosZ, out IntPtr listener);
+
+    delegate void TNewShaderParametersCallback(
+        in CG3DShaderParameters shaderParameters, out IntPtr listener);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int result = initLibrary();
+        Debug.Log("initLibrary Result: " + result);
+
+        int deviceCount = -1;
+        result = getHeadTrackingDeviceCount(out deviceCount);
+        Debug.Log("getHeadTrackingDeviceCount Result: " + result);
+
+        result = deinitLibrary();
+        Debug.Log("deinitLibrary Result: " + result);
+        // int deviceCount = 0;
+        // int result = getHeadTrackingDeviceCount(deviceCount);
+        // Debug.Log("Result: " + result);
+        // Debug.Log("Device Count: " + deviceCount);
+        // IntPtr ptr =  new IntPtr();
+        // result = getHeadTrackingDeviceName(0, ptr);
+        // Debug.Log("Result: " + result);
+
+        // string deviceName = Marshal.PtrToStringAnsi(ptr);
+        // Debug.Log("Device Name: " + deviceName);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
