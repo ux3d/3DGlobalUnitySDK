f6b3d918b8eedf1227b02bb4450f6b2fa640ad27
Phillip Hohenester <hohenester@ux3d.io>
2024-11-25 16:19:11 +0100

    began work on mosaic video for multiview support

---
+++ Resources/G3DShaderMultiviewMosaic.shader
@@ -0,0 +1,271 @@
+Shader "G3D/AutostereoMultiviewMosaic"
+{
+    HLSLINCLUDE
+    #pragma target 4.5
+    #pragma only_renderers d3d11 playstation xboxone vulkan metal switch
+
+    int  nativeViewCount;      // Anzahl nativer Views
+    int  zwinkel;        // Winkelzähler
+    int  nwinkel;        // Winkelnenner
+    int  isleft;         // links(1) oder rechts(0) geneigtes Lentikular
+    int  test;           // Rot/Schwarz (1)ein, (0)aus
+    int  hqview;         // hqhqViewCount
+    
+    
+    int  s_height;       // screen height
+    int  v_pos_x;        // horizontal viewport position
+    int  v_pos_y;        // vertical viewport position
+    
+    // This shader was originally implemented for OpenGL, so we need to invert the y axis to make it work in Unity.
+    // to do this we need the actual viewport height
+    int viewportHeight;
+    
+    // amount of render targets
+    int cameraCount;
+    
+    int mirror; // 1: mirror from left to right, 0: no mirror
+    
+    // unused parameter -> only here for so that this shader overlaps with the multiview shader
+    int isBGR; // 0 = RGB, 1 = BGR
+
+    // mosaic video parameters
+    int mosaic_rows; // number of rows in the mosaic
+    int mosaic_columns; // number of columns in the mosaic
+    
+    struct v2f
+    {
+        float2 uv : TEXCOORD0;
+        float4 screenPos : SV_POSITION;
+    };
+
+
+    Texture2D mosaictexture;
+    SamplerState samplermosaictexture;
+
+    // original code (adapted to remove redundant calculations):
+    // map(viewIndex, 0, calculatedViewCount, 0, cameraCount);
+    // float map(float s, float from1, float from2, float to1, float to2)
+    // {
+    //     return to1 + (s-from1)*(to2-to1)/(from2-from1);
+    // }
+    float map(float s, float from2, float to2)
+    {
+        return s*to2/from2;
+    }
+
+    float2 calculateUVForMosaic(int index, float2 startingUV) {
+        int2 moasicIndex = int2(index % mosaic_rows, index / mosaic_rows);
+        float2 uv = float2(0, 0);
+        float2 scaledUV = float2(startingUV.x / mosaic_columns, startingUV.y / mosaic_rows);
+        float2 cellSize = float2(1.0 / mosaic_columns, 1.0 / mosaic_rows);
+        uv = scaledUV + cellSize * moasicIndex;
+        return uv;
+    }
+
+    float4 frag (v2f i) : SV_Target
+    {
+        float yPos = s_height - i.screenPos.y; // invert y coordinate to account for different coordinates between glsl and hlsl (original shader written in glsl)
+        float2 computedScreenPos = float2(i.screenPos.x, yPos) + float2(v_pos_x, v_pos_y);
+        int calculatedViewCount = nativeViewCount * nwinkel;
+        /*
+        1) startIndex
+            each horizontal line starts with a different view index:
+                (y * uzwinkel) % max_views
+                e.g.: uzwinkel = 4 and max_views = 35: 34,3,7,11,15,19,23,27,31,0,4,...
+            depending on the screen, this view index will increase or decrease in its modulo loop, therefore:
+                uDirection (either -1 or 1)
+            handling negative modulo mod(a,b) means we need to add b IF the result is negative, but we dont want that IF, therefore:
+                + y * calculatedViewCount
+            each step on the horizontal line in x increases the startIndex by wn (on a subpixel level), therefore:
+                startIndex + xScreenCoords * 3 * unwinkel
+        */
+        // shift direction from 0 or 1 to -1 or 1
+        // int direction = (isleft + 1) * 2 - 3;
+        // * 3 bacause we have three color chanels
+        int direction = (isleft + 1) * 2 - 3;
+        int startIndex = computedScreenPos.y * zwinkel * (direction * -1) +
+            computedScreenPos.y * calculatedViewCount + computedScreenPos.x * 3 * nwinkel;
+
+        /*
+        2) viewIndex
+            in a shader we operate on a color level tho, so we need to add wn:
+                + float3(0, unwinkel, 2*unwinkel)
+            offsets from the UI or headtracking can factor into this as well:
+                view_offset + view_offset_headtracking
+            of course, this value will be out of range for our views, so we need a modulo operation that can only behave as expected
+                glsl_mod(float3, float) = f - floor(f / m) * m;
+        */
+        int3 viewIndices = int3(startIndex, startIndex, startIndex);
+        viewIndices += int3(0, nwinkel, nwinkel + nwinkel);
+        viewIndices = viewIndices % calculatedViewCount;
+
+        float2 uvCoords = i.uv;
+        // mirror the image if necessary
+        if (mirror != 0) {
+            uvCoords.x = 1.0 - uvCoords.x;
+        }
+
+        //use indices to sample correct subpixels
+        float4 color = float4(0.0, 0.0, 0.0, 1.0);
+        float4 tmpColor = mosaictexture.Sample(samplermosaictexture, i.uv);
+        return tmpColor;
+        int viewIndex = 0;
+        for (int channel = 0; channel < 3; channel++) {
+            if(isBGR != 0) {
+                viewIndex = viewIndices[2 - channel];
+            } else {
+                viewIndex = viewIndices[channel];
+            }
+
+            if (test != 0) {
+                if (viewIndex == 0) {
+                    color[channel] = 1.0;
+                }
+                continue;
+            }
+
+            float mappedIndex = map(viewIndex, calculatedViewCount, cameraCount);
+            uvCoords = calculateUVForMosaic(mappedIndex, uvCoords);
+            float4 tmpColor = mosaictexture.Sample(samplermosaictexture, uvCoords);
+
+            if(channel == 0) {
+                color.x = tmpColor.x;
+            } else if(channel == 1) {
+                color.y = tmpColor.y;
+            } else if(channel == 2) {
+                color.z = tmpColor.z;
+            }
+        }
+        
+        return color;
+    }
+    ENDHLSL
+
+    // URP Shader
+    SubShader
+    {
+        PackageRequirements
+        {
+            "com.unity.render-pipelines.universal": "unity=2021.3"
+        }
+
+        Tags { "RenderType"="Opaque" "RenderPipeline" = "UniversalPipeline"}
+        LOD 100
+        ZWrite Off Cull Off
+
+        Pass
+        {
+            ZWrite Off
+            ZTest Always
+            Blend Off
+            Cull Off
+
+            HLSLPROGRAM
+                #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+
+                #pragma vertex vert
+                #pragma fragment frag
+
+                struct VertAttributes
+                {
+                    uint vertexID : SV_VertexID;
+                    UNITY_VERTEX_INPUT_INSTANCE_ID
+                };
+
+                v2f vert(VertAttributes input)
+                {
+                    v2f output;
+                    UNITY_SETUP_INSTANCE_ID(input);
+                    UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
+                    output.uv = GetFullScreenTriangleTexCoord(input.vertexID);
+                    output.screenPos = GetFullScreenTriangleVertexPosition(input.vertexID);
+
+                    return output;
+
+                }
+            ENDHLSL
+        }
+    }
+
+    // HDRP Shader
+    SubShader
+    {
+        PackageRequirements
+        {
+            "com.unity.render-pipelines.high-definition": "unity=2021.3"
+        }
+
+        Tags { "RenderType"="Opaque" "RenderPipeline" = "HDRenderPipeline"}
+
+        Pass
+        {
+            Name "G3DFullScreen3D"
+            
+            ZWrite Off
+            ZTest Always
+            Blend Off
+            Cull Off
+
+            HLSLPROGRAM
+                #include "Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/RenderPass/CustomPass/CustomPassCommon.hlsl"
+                #include "Packages/com.unity.render-pipelines.high-definition/Runtime/ShaderLibrary/ShaderVariables.hlsl"
+
+                #pragma vertex vert
+                #pragma fragment frag
+
+                struct VertAttributes
+                {
+                    uint vertexID : SV_VertexID;
+                    UNITY_VERTEX_INPUT_INSTANCE_ID
+                };
+
+                v2f vert(VertAttributes input)
+                {
+                    v2f output;
+                    UNITY_SETUP_INSTANCE_ID(input);
+                    UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
+                    output.uv = GetFullScreenTriangleTexCoord(input.vertexID);
+                    output.screenPos = GetFullScreenTriangleVertexPosition(input.vertexID);
+
+                    return output;
+
+                }
+            ENDHLSL
+        }
+    }
+
+    // Built-in Render Pipeline
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment fragSRP
+
+            #include "UnityCG.cginc"
+
+            v2f vert(float4 vertex : POSITION, float2 uv : TEXCOORD0)
+            {
+                v2f o;
+                o.uv = uv;
+                o.screenPos = UnityObjectToClipPos(vertex);
+                return o;
+            }
+
+
+            float4 fragSRP(v2f i) : SV_Target
+            {
+                // invert y axis to account for different coordinate systems between Unity and OpenGL (OpenGL has origin at bottom left)
+                // The shader was written for OpenGL, so we need to invert the y axis to make it work in Unity.
+                i.screenPos.y = viewportHeight - i.screenPos.y;
+                return frag(i);
+            }
+
+            ENDHLSL
+        }
+    }
+}
---
+++ Scripts/G3DCameraMosaicMultiview.cs
@@ -0,0 +1,367 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+#if UNITY_EDITOR
+using UnityEditor.EditorTools;
+using UnityEngine.Video;
+
+#endif
+
+#if HDRP
+using UnityEngine.Rendering.HighDefinition;
+#endif
+
+#if URP
+using UnityEngine.Rendering.Universal;
+#endif
+
+/// <summary>
+/// Replaces the image the camera this script is attached to sees with the rendertexture.
+/// The texture should contain a mosaic image with several views.
+///
+/// IMPORTANT: This script must not be attached to a camera already using a G3D camera script.
+/// </summary>
+[RequireComponent(typeof(Camera))]
+public class G3DCameraMosaicMultiview : MonoBehaviour
+{
+    #region Calibration
+
+    [Tooltip(
+        "If not null the initial set of shader parameters will be based on this calibration file. If null the default shader parameters from the library will be used."
+    )]
+    public string customDefaultCalibrationFilePath = null;
+    #endregion
+
+    public RenderTexture mosaicTexture;
+
+    #region 3D Effect settings
+    [Header("3D Effect settings")]
+    [Tooltip(
+        "If set to true, the amount of cameras will be limited by the amount of native views the display supports."
+    )]
+    public bool lockCameraCountToDisplay = false;
+
+    [Tooltip("If set to true, the views will be flipped horizontally.")]
+    public bool mirrorViews = false;
+    #endregion
+
+    #region Debugging
+    [Header("Debugging")]
+    public bool showTestFrame = false;
+
+    #endregion
+
+    #region Keys
+    [Header("Keys")]
+    [Tooltip("If set to true, the library will react to certain keyboard keys.")]
+    public bool enableKeys = true;
+    public KeyCode shiftViewLeftKey = KeyCode.LeftArrow;
+    public KeyCode shiftViewRightKey = KeyCode.RightArrow;
+
+    [Tooltip("Shows a red/green test frame.")]
+    public KeyCode toggleTestFrameKey = KeyCode.D;
+    #endregion
+
+    #region Private variables
+
+    private Camera mainCamera;
+    private Material material;
+#if HDRP
+    private G3DHDRPCustomPass customPass;
+#endif
+#if URP
+    private G3DUrpScriptableRenderPass customPass;
+#endif
+
+    private ShaderHandles shaderHandles;
+    private G3DShaderParameters shaderParameters;
+
+    private Vector2Int cachedWindowPosition;
+    private Vector2Int cachedWindowSize;
+
+    [Min(1)]
+    public int mosaicRowCount = 3;
+
+    [Min(1)]
+    public int mosaicColumnCount = 3;
+
+    #endregion
+
+    // TODO Handle viewport resizing/ moving
+
+    #region Initialization
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+        mainCamera.cullingMask = 0; //disable rendering of the main camera
+        mainCamera.clearFlags = CameraClearFlags.Color;
+
+        //initialize cameras
+
+        shaderHandles = new ShaderHandles()
+        {
+            leftViewportPosition = Shader.PropertyToID("v_pos_x"),
+            bottomViewportPosition = Shader.PropertyToID("v_pos_y"),
+            screenHeight = Shader.PropertyToID("s_height"),
+            nativeViewCount = Shader.PropertyToID("nativeViewCount"),
+            angleRatioNumerator = Shader.PropertyToID("zwinkel"),
+            angleRatioDenominator = Shader.PropertyToID("nwinkel"),
+            leftLensOrientation = Shader.PropertyToID("isleft"),
+            BGRPixelLayout = Shader.PropertyToID("isBGR"),
+            showTestFrame = Shader.PropertyToID("test"),
+            hqViewCount = Shader.PropertyToID("hqview"),
+        };
+
+#if HDRP
+        // init fullscreen postprocessing for hd render pipeline
+        var customPassVolume = gameObject.AddComponent<CustomPassVolume>();
+        customPassVolume.injectionPoint = CustomPassInjectionPoint.AfterPostProcess;
+        customPassVolume.isGlobal = true;
+        // Make the volume invisible in the inspector
+        customPassVolume.hideFlags = HideFlags.HideInInspector | HideFlags.DontSave;
+        customPass = customPassVolume.AddPassOfType(typeof(G3DHDRPCustomPass)) as G3DHDRPCustomPass;
+        customPass.fullscreenPassMaterial = material;
+        customPass.materialPassName = "G3DFullScreen3D";
+#endif
+
+#if URP
+        customPass = new G3DUrpScriptableRenderPass(material);
+#endif
+
+        reinitializeShader();
+        updateScreenViewportProperties();
+
+        DefaultCalibrationProvider defaultCalibrationProvider =
+            DefaultCalibrationProvider.getFromConfigFile(customDefaultCalibrationFilePath);
+        shaderParameters = defaultCalibrationProvider.getDefaultShaderParameters();
+        updateShaderParameters();
+
+        // This has to be done after the cameras are updated
+        cachedWindowPosition = new Vector2Int(
+            Screen.mainWindowPosition.x,
+            Screen.mainWindowPosition.y
+        );
+        cachedWindowSize = new Vector2Int(Screen.width, Screen.height);
+
+        // VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
+        // uint width = videoPlayer.width;
+        // uint height = videoPlayer.height;
+        // mosaicTexture = new RenderTexture((int)width, (int)height, 0);
+        // GetComponent<VideoPlayer>().targetTexture = mosaicTexture;
+    }
+
+    private void reinitializeShader()
+    {
+        material = new Material(Shader.Find("G3D/AutostereoMultiviewMosaic"));
+        material.SetTexture("mosaictexture", mosaicTexture, RenderTextureSubElement.Color);
+    }
+
+#if URP
+    private void OnEnable()
+    {
+        RenderPipelineManager.beginCameraRendering += OnBeginCamera;
+    }
+
+    private void OnDisable()
+    {
+        RenderPipelineManager.beginCameraRendering -= OnBeginCamera;
+    }
+
+    private void OnBeginCamera(ScriptableRenderContext context, Camera cam)
+    {
+        // Use the EnqueuePass method to inject a custom render pass
+        cam.GetUniversalAdditionalCameraData().scriptableRenderer.EnqueuePass(customPass);
+    }
+#endif
+
+    #endregion
+
+    #region Updates
+    void Update()
+    {
+        updateShaderParameters();
+
+        if (enableKeys)
+        {
+            handleKeyPresses();
+        }
+
+        if (windowResized() || windowMoved())
+        {
+            updateScreenViewportProperties();
+        }
+    }
+
+    private void updateScreenViewportProperties()
+    {
+        try
+        {
+            shaderParameters.screenHeight = Screen.height;
+            shaderParameters.screenWidth = Screen.width;
+            shaderParameters.leftViewportPosition = Screen.mainWindowPosition.x;
+            shaderParameters.bottomViewportPosition = Screen.mainWindowPosition.y + Screen.height;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Failed to update screen viewport properties: " + e.Message);
+        }
+
+        // this parameter is used in the shader to invert the y axis
+        material?.SetInt(Shader.PropertyToID("viewportHeight"), Screen.height);
+    }
+
+    private void updateShaderParameters()
+    {
+        material?.SetInt(shaderHandles.leftViewportPosition, shaderParameters.leftViewportPosition);
+        material?.SetInt(
+            shaderHandles.bottomViewportPosition,
+            shaderParameters.bottomViewportPosition
+        );
+        material?.SetInt(shaderHandles.screenHeight, shaderParameters.screenHeight);
+        material?.SetInt(shaderHandles.nativeViewCount, shaderParameters.nativeViewCount);
+        material?.SetInt(shaderHandles.angleRatioNumerator, shaderParameters.angleRatioNumerator);
+        material?.SetInt(
+            shaderHandles.angleRatioDenominator,
+            shaderParameters.angleRatioDenominator
+        );
+        material?.SetInt(shaderHandles.leftLensOrientation, shaderParameters.leftLensOrientation);
+        material?.SetInt(shaderHandles.showTestFrame, shaderParameters.showTestFrame);
+        material?.SetInt(shaderHandles.hqViewCount, shaderParameters.hqViewCount);
+        material?.SetInt(shaderHandles.BGRPixelLayout, shaderParameters.BGRPixelLayout);
+
+        int cameraCount = mosaicColumnCount * mosaicRowCount;
+        int shaderMaxCount = shaderParameters.nativeViewCount;
+        if (cameraCount > shaderMaxCount && lockCameraCountToDisplay)
+        {
+            cameraCount = shaderMaxCount;
+        }
+
+        material?.SetInt(Shader.PropertyToID("cameraCount"), cameraCount);
+
+        material?.SetInt(Shader.PropertyToID("mirror"), mirrorViews ? 1 : 0);
+
+        material?.SetInt(Shader.PropertyToID("mosaic_rows"), mosaicRowCount);
+        material?.SetInt(Shader.PropertyToID("mosaic_columns"), mosaicColumnCount);
+    }
+
+    private bool windowResized()
+    {
+        var window_dim = new Vector2Int(Screen.width, Screen.height);
+        if (cachedWindowSize != window_dim)
+        {
+            cachedWindowSize = window_dim;
+            return true;
+        }
+        return false;
+    }
+
+    private bool windowMoved()
+    {
+        var window_pos = new Vector2Int(Screen.mainWindowPosition.x, Screen.mainWindowPosition.y);
+        if (cachedWindowPosition != window_pos)
+        {
+            cachedWindowPosition = window_pos;
+            return true;
+        }
+        return false;
+    }
+
+    private void handleKeyPresses()
+    {
+        if (Input.GetKeyDown(shiftViewLeftKey))
+        {
+            try
+            {
+                // TODO emulate
+                // libInterface.shiftViewToLeft();
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Failed to shift view to left: " + e.Message);
+            }
+        }
+        if (Input.GetKeyDown(shiftViewRightKey))
+        {
+            try
+            {
+                // TODO emulate
+                // libInterface.shiftViewToRight();
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Failed to shift view to right: " + e.Message);
+            }
+        }
+        if (Input.GetKeyDown(toggleTestFrameKey))
+        {
+            showTestFrame = !showTestFrame;
+        }
+    }
+
+    // This function only does something when you use the SRP render pipeline.
+    // when using either URP or HRDP image combination is handled in the respective renderpasses.
+    // URP -> G3DUrpScriptableRenderPass.cs
+    // HDRP -> G3DHDRPCustomPass.cs
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        // This is where the material and shader are applied to the camera image.
+        //legacy support (no URP or HDRP)
+#if HDRP || URP
+#else
+        if (material == null)
+            Graphics.Blit(source, destination);
+        else
+            Graphics.Blit(source, destination, material);
+#endif
+    }
+    #endregion
+
+    /// <summary>
+    /// The provided file path has to be a display calibration ini file.
+    /// </summary>
+    /// <param name="filePath"></param>
+    public void UpdateShaderParametersFromFile(string filePath)
+    {
+        if (filePath == null || filePath == "" || filePath.EndsWith(".ini") == false)
+        {
+            return;
+        }
+
+        try
+        {
+            DefaultCalibrationProvider defaultCalibrationProvider =
+                DefaultCalibrationProvider.getFromConfigFile(filePath);
+            shaderParameters = defaultCalibrationProvider.getDefaultShaderParameters();
+            updateShaderParameters();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Failed to update shader parameters from file: " + e.Message);
+        }
+    }
+
+    /// <summary>
+    /// The provided string has to be a display calibration ini file.
+    /// </summary>
+    /// <param name="json"></param>
+    public void UpdateShaderParametersFromINIString(string iniFile)
+    {
+        if (iniFile == null || iniFile == "")
+        {
+            return;
+        }
+
+        try
+        {
+            DefaultCalibrationProvider defaultCalibrationProvider =
+                DefaultCalibrationProvider.getFromString(iniFile);
+            shaderParameters = defaultCalibrationProvider.getDefaultShaderParameters();
+            updateShaderParameters();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Failed to update shader parameters from json: " + e.Message);
+        }
+    }
+}
