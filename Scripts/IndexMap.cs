6f068853a84bb8e60ffdd5dd771d3de71467355c
Phillip Hohenester <hohenester@ux3d.io>
2026-01-13 16:37:12 +0100

    improved index map script

--- Scripts/IndexMap.cs
+++ Scripts/IndexMap.cs
@@ -3,43 +3,56 @@ using System.Collections.Generic;
 
 public class IndexMap
 {
+    private static readonly IndexMap internalInstance = new IndexMap();
+
+    // Explicit static constructor to tell C# compiler
+    // not to mark type as beforefieldinit
+    static IndexMap()
+    {
+        internalInstance.generateDefaultMaps();
+    }
+
+    public static IndexMap Instance
+    {
+        get { return internalInstance; }
+    }
+
     private int BLANK_VIEW = 250; // corresponds to a black view
-    public Dictionary<string, byte[]> maps;
-    List<int> generatedMap;
+    public Dictionary<string, int[]> maps;
 
     public void generateDefaultMaps()
     {
-        maps = new Dictionary<string, List<int>>();
+        maps = new Dictionary<string, int[]>();
 
         string name = "A";
         int[] mapArray = new int[35];
         generate_indexmap(ref mapArray, mapArray.Length, 2, 0.0f, 1.0f, false);
-        maps[name] = List.FromArray(mapArray);
+        maps[name] = mapArray;
 
         name = "B";
         mapArray = new int[35];
         generate_indexmap(ref mapArray, mapArray.Length, 6, 0.0f, 1.0f, false);
-        maps[name] = List.FromArray(mapArray);
+        maps[name] = mapArray;
 
         name = "C";
         mapArray = new int[35];
         generate_indexmap(ref mapArray, mapArray.Length, 8, 0.0f, 1.0f, false);
-        maps[name] = List.FromArray(mapArray);
+        maps[name] = mapArray;
 
         name = "D";
         mapArray = new int[35];
         generate_indexmap(ref mapArray, mapArray.Length, 9, 0.0f, 1.0f, false);
-        maps[name] = List.FromArray(mapArray);
+        maps[name] = mapArray;
 
         name = "E";
         mapArray = new int[35];
         generate_indexmap(ref mapArray, mapArray.Length, 16, 0.0f, 1.0f, false);
-        maps[name] = List.FromArray(mapArray);
+        maps[name] = mapArray;
 
         name = "S1D";
         maps.Add(
             name,
-            new List<int>
+            new int[]
             {
                 250,
                 250,
@@ -82,7 +95,7 @@ public class IndexMap
         name = "S2D";
         maps.Add(
             name,
-            new List<int>
+            new int[]
             {
                 250,
                 250,
@@ -125,7 +138,7 @@ public class IndexMap
         name = "S3D";
         maps.Add(
             name,
-            new List<int>
+            new int[]
             {
                 250,
                 250,
@@ -168,7 +181,7 @@ public class IndexMap
         name = "S4D";
         maps.Add(
             name,
-            new List<int>
+            new int[]
             {
                 250,
                 250,
@@ -211,7 +224,7 @@ public class IndexMap
         name = "S6D";
         maps.Add(
             name,
-            new List<int>
+            new int[]
             {
                 250,
                 250,
@@ -242,66 +255,36 @@ public class IndexMap
         );
 
         name = "05A";
-        maps.Add(name, new List<int> { 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4 });
+        maps.Add(name, new int[] { 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4 });
 
         name = "05C";
-        maps.Add(name, new List<int> { 0, 1, 2, 3, 4 });
+        maps.Add(name, new int[] { 0, 1, 2, 3, 4 });
 
         name = "05N";
-        maps.Add(name, new List<int> { 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4 });
+        maps.Add(name, new int[] { 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4 });
 
         name = "05O";
-        maps.Add(name, new List<int> { 0, 1, 2, 3, 4 });
+        maps.Add(name, new int[] { 0, 1, 2, 3, 4 });
 
         name = "05P";
-        maps.Add(name, new List<int> { 0, 1, 2, 3, 4 });
+        maps.Add(name, new int[] { 0, 1, 2, 3, 4 });
 
         name = "05Q";
         maps.Add(
             name,
-            new List<int>
-            {
-                0,
-                0,
-                0,
-                0,
-                0,
-                1,
-                1,
-                1,
-                1,
-                1,
-                2,
-                2,
-                2,
-                2,
-                2,
-                3,
-                3,
-                3,
-                3,
-                3,
-                4,
-                4,
-                4,
-                4,
-                4
-            }
+            new int[] { 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4 }
         );
 
         name = "05R";
-        maps.Add(name, new List<int> { 0, 1, 2, 3, 4 });
+        maps.Add(name, new int[] { 0, 1, 2, 3, 4 });
 
         name = "07A";
-        maps.Add(
-            name,
-            new List<int> { 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6 }
-        );
+        maps.Add(name, new int[] { 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6 });
 
         name = "07D";
         maps.Add(
             name,
-            new List<int>
+            new int[]
             {
                 0,
                 0,
@@ -344,16 +327,16 @@ public class IndexMap
         name = "08A";
         maps.Add(
             name,
-            new List<int> { 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7 }
+            new int[] { 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7 }
         );
 
         name = "08B";
-        maps.Add(name, new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 });
+        maps.Add(name, new int[] { 0, 1, 2, 3, 4, 5, 6, 7 });
 
         name = "08D";
         maps.Add(
             name,
-            new List<int>
+            new int[]
             {
                 0,
                 0,
@@ -399,12 +382,12 @@ public class IndexMap
         );
 
         name = "08O";
-        maps.Add(name, new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 });
+        maps.Add(name, new int[] { 0, 1, 2, 3, 4, 5, 6, 7 });
 
         name = "08Q";
         maps.Add(
             name,
-            new List<int>
+            new int[]
             {
                 0,
                 0,
@@ -452,7 +435,7 @@ public class IndexMap
         name = "120views";
         maps.Add(
             name,
-            new List<int>
+            new int[]
             {
                 0,
                 1,
@@ -580,19 +563,19 @@ public class IndexMap
         name = "A8A";
         maps.Add(
             name,
-            new List<int> { 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7 }
+            new int[] { 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7 }
         );
 
         name = "A8B";
-        maps.Add(name, new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 });
+        maps.Add(name, new int[] { 0, 1, 2, 3, 4, 5, 6, 7 });
 
         name = "A8O";
-        maps.Add(name, new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 });
+        maps.Add(name, new int[] { 0, 1, 2, 3, 4, 5, 6, 7 });
 
         name = "IPRO_CUS01";
         maps.Add(
             name,
-            new List<int>
+            new int[]
             {
                 0,
                 77,
@@ -622,43 +605,45 @@ public class IndexMap
         ); // 77 == blank? if so: 77 = 250!
     }
 
-    public void generate_indexmap(
+    private void generate_indexmap(
         ref int[] indexmap,
-        int available_views,
-        int content_views,
-        float delimiter_views,
-        float invert_start,
-        bool invert
+        int availableViews,
+        int contentViews,
+        float delimiterViews,
+        float yoyoStart,
+        bool invert,
+        bool invertIndices = false
     )
     {
-        if (available_views <= 1 || content_views == 1)
+        // sanitize inputs
+        yoyoStart = Math.Clamp(yoyoStart, 0.0f, 1.0f);
+        delimiterViews = Math.Clamp(delimiterViews, 0.0f, 1.0f);
+
+        if (availableViews <= 1 || contentViews == 1)
         {
-            for (int j = 0; j < available_views; j++)
+            for (int j = 0; j < availableViews; j++)
                 indexmap[j] = 0;
             return;
         }
 
-        int i = 0;
-        if (invert_start < 1.0f)
+        if (yoyoStart < 1.0f)
         {
-            int nonInvertedCount = (int)(available_views * invert_start);
+            int nonInvertedCount = (int)(availableViews * yoyoStart);
             int[] indexmapNonInvertedPart = new int[nonInvertedCount];
-            int[] indexmapInvertedPart = new int[available_views - nonInvertedCount];
+            int[] indexmapInvertedPart = new int[availableViews - nonInvertedCount];
 
-            generate_indexmap(
+            generateAutomaticDistribution(
                 ref indexmapNonInvertedPart,
                 nonInvertedCount,
-                content_views,
+                contentViews,
                 0.0f,
-                1.0f,
                 false
             );
-            generate_indexmap(
+            generateAutomaticDistribution(
                 ref indexmapInvertedPart,
-                available_views - nonInvertedCount,
-                content_views,
+                availableViews - nonInvertedCount,
+                contentViews,
                 0.0f,
-                1.0f,
                 true
             );
             Array.Copy(indexmapNonInvertedPart, 0, indexmap, 0, nonInvertedCount);
@@ -667,90 +652,231 @@ public class IndexMap
                 0,
                 indexmap,
                 nonInvertedCount,
-                available_views - nonInvertedCount
+                availableViews - nonInvertedCount
             );
         }
         else
         {
-            // automatic distribution
-            if (content_views == 2)
+            generateAutomaticDistribution(
+                ref indexmap,
+                availableViews,
+                contentViews,
+                delimiterViews,
+                invert
+            );
+        }
+
+        if (invert)
+        {
+            int x = 0;
+            int y = availableViews - 1;
+            int tmp;
+            while (x < y)
             {
-                float eyeAreaSpace = (1.0f - delimiter_views) / 2;
-
-                int delimViews = (int)(available_views * delimiter_views);
-                int eyeViews = (int)(available_views * eyeAreaSpace);
-                int startDelimViews = (available_views - delimViews - eyeViews * 2) / 2;
-                int endDelimViews = available_views - delimViews - eyeViews * 2 - startDelimViews;
-
-                for (; startDelimViews > 0; startDelimViews--)
-                    indexmap[i++] = BLANK_VIEW;
-                for (int j = 0; j < eyeViews; j++)
-                    indexmap[i++] = 1;
-                for (; delimViews > 0; delimViews--)
-                    indexmap[i++] = BLANK_VIEW;
-                for (int j = 0; j < eyeViews; j++)
-                    indexmap[i++] = 0;
-                for (; endDelimViews > 0; endDelimViews--)
-                    indexmap[i++] = BLANK_VIEW;
+                tmp = indexmap[x];
+                indexmap[x] = indexmap[y];
+                indexmap[y] = tmp;
+                x++;
+                y--;
             }
-            else
-            {
-                int delimViewsStart = (int)(available_views * delimiter_views / 2);
-                int delimViewsEnd = (int)(available_views * delimiter_views / 2);
-                int spacePerView =
-                    (available_views - delimViewsStart - delimViewsEnd) / content_views;
-                int spaceLeftover =
-                    available_views
-                    - delimViewsStart
-                    - delimViewsEnd
-                    - spacePerView * content_views;
+        }
 
-                for (; delimViewsStart > 0; delimViewsStart--)
-                    indexmap[i++] = BLANK_VIEW;
-                for (int viewIndex = 0; viewIndex < content_views; viewIndex++)
-                {
-                    int offset = 0;
-                    if (spaceLeftover > 0)
-                    {
-                        offset = 1;
-                        spaceLeftover--;
-                    }
-                    for (int currentSpace = spacePerView + offset; currentSpace > 0; currentSpace--)
-                    {
-                        indexmap[i++] = content_views - 1 - viewIndex;
-                    }
-                }
-                for (; delimViewsEnd > 0; delimViewsEnd--)
-                    indexmap[i++] = BLANK_VIEW;
-            }
-            for (int j = i; j < available_views; j++)
-                indexmap[j] = 0;
-
-            if (invert)
+        if (invertIndices)
+        {
+            for (int i = 0; i < indexmap.Length; i++)
             {
-                int x = 0;
-                int y = available_views - 1;
-                int tmp;
-                while (x < y)
+                if (indexmap[i] != BLANK_VIEW)
                 {
-                    tmp = indexmap[x];
-                    indexmap[x] = indexmap[y];
-                    indexmap[y] = tmp;
-                    x++;
-                    y--;
-                }
-            }
-
-            if (delimiter_views == 0.0f)
-            {
-                if (indexmap[0] == BLANK_VIEW)
-                    indexmap[0] = 0;
-                for (int x = 1; x < available_views; x++)
-                {
-                    if (indexmap[x] == BLANK_VIEW)
-                        indexmap[x] = indexmap[x - 1];
+                    indexmap[i] = contentViews - 1 - indexmap[i];
                 }
             }
         }
+    }
+
+    private void generateAutomaticDistribution(
+        ref int[] indexmap,
+        int availableViews,
+        int contentViews,
+        float delimiterViews,
+        bool invert
+    )
+    {
+        if (indexmap.Length == 0)
+            return;
+
+        int i;
+        // automatic distribution
+        if (contentViews == 2)
+        {
+            i = generateTwoContentViews(ref indexmap, availableViews, delimiterViews);
+        }
+        else
+        {
+            i = generateMoreThanTwoContentViews(
+                ref indexmap,
+                availableViews,
+                contentViews,
+                delimiterViews
+            );
+        }
+        for (int j = i; j < availableViews; j++)
+            indexmap[j] = 0;
+
+        if (invert)
+        {
+            int x = 0;
+            int y = availableViews - 1;
+            int tmp;
+            while (x < y)
+            {
+                tmp = indexmap[x];
+                indexmap[x] = indexmap[y];
+                indexmap[y] = tmp;
+                x++;
+                y--;
+            }
+        }
+
+        if (delimiterViews == 0.0f)
+        {
+            if (indexmap[0] == BLANK_VIEW)
+                indexmap[0] = 0;
+            for (int x = 1; x < availableViews; x++)
+            {
+                if (indexmap[x] == BLANK_VIEW)
+                    indexmap[x] = indexmap[x - 1];
+            }
+        }
+    }
+
+    private int generateTwoContentViews(
+        ref int[] indexmap,
+        int availableViews,
+        float delimiterViews
+    )
+    {
+        int i = 0;
+        float eyeAreaSpace = (1.0f - delimiterViews) / 2;
+
+        int delimViews = (int)(availableViews * delimiterViews);
+        int eyeViews = (int)(availableViews * eyeAreaSpace);
+        int startDelimViews = (availableViews - delimViews - eyeViews * 2) / 2;
+        int endDelimViews = availableViews - delimViews - eyeViews * 2 - startDelimViews;
+
+        for (; startDelimViews > 0; startDelimViews--)
+            indexmap[i++] = BLANK_VIEW;
+        for (int j = 0; j < eyeViews; j++)
+            indexmap[i++] = 1;
+        for (; delimViews > 0; delimViews--)
+            indexmap[i++] = BLANK_VIEW;
+        for (int j = 0; j < eyeViews; j++)
+            indexmap[i++] = 0;
+        for (; endDelimViews > 0; endDelimViews--)
+            indexmap[i++] = BLANK_VIEW;
+
+        return i;
+    }
+
+    private int generateMoreThanTwoContentViews(
+        ref int[] indexmap,
+        int availableViews,
+        int contentViews,
+        float delimiterViews
+    )
+    {
+        int i = 0;
+        int delimViewsStart = (int)(availableViews * delimiterViews / 2);
+        int delimViewsEnd = (int)(availableViews * delimiterViews / 2);
+        int spacePerView = (availableViews - delimViewsStart - delimViewsEnd) / contentViews;
+        int spaceLeftover =
+            availableViews - delimViewsStart - delimViewsEnd - spacePerView * contentViews;
+
+        for (; delimViewsStart > 0; delimViewsStart--)
+            indexmap[i++] = BLANK_VIEW;
+        for (int viewIndex = 0; viewIndex < contentViews; viewIndex++)
+        {
+            int offset = 0;
+            if (spaceLeftover > 0)
+            {
+                offset = 1;
+                spaceLeftover--;
+            }
+            for (int currentSpace = spacePerView + offset; currentSpace > 0; currentSpace--)
+            {
+                indexmap[i++] = (int)(contentViews - 1 - viewIndex);
+            }
+        }
+        for (; delimViewsEnd > 0; delimViewsEnd--)
+            indexmap[i++] = BLANK_VIEW;
+
+        return i;
+    }
+
+    /// <summary>
+    /// Creates a new index map and adds it to the internal dictionary with the given key.
+    /// DANGEROUS: if the key already exists, the existing map will not be overwritten
+    /// if the parameter overwrite is false!
+    /// </summary>
+    /// <param name="key">key to the internal map.</param>
+    /// <param name="availableViews">number of available views on the display. has to be larger than 0</param>
+    /// <param name="contentViews">number of views your content has. has to be larger than 0</param>
+    /// <param name="yoyoStart">set the percentage where the inversion will start (e.g. 0, 1,2,3,2,1,0); will be clamped between 0.0 and 1.0</param>
+    /// <param name="invert">if true, the final index map will be inverted</param>
+    /// <param name="overwrite">if true, the internal map in the internal dictionary will be overwritten</param>
+    /// <returns></returns>
+    public int[] GetIndexMap(
+        string key,
+        int availableViews,
+        int contentViews,
+        float yoyoStart,
+        bool invert,
+        bool invertIndices = false,
+        bool overwrite = false
+    )
+    {
+        if (maps.ContainsKey(key) && !overwrite)
+        {
+            return maps[key]; // key found, return the loaded map
+        }
+
+        int[] mapArray = GetIndexMapTemp(
+            availableViews,
+            contentViews,
+            yoyoStart,
+            invert,
+            invertIndices
+        );
+        maps[key] = mapArray;
+        // key not found -> generate a map and return a pointer to that one
+        return mapArray;
+    }
+
+    /// <summary>
+    /// Creates a new index map without storing it in the internal dictionary.
+    /// </summary>
+    /// <param name="availableViews">number of available views on the display. has to be larger than 0</param>
+    /// <param name="contentViews">number of views your content has. has to be larger than 0</param>
+    /// <param name="yoyoStart">set the percentage where the inversion will start (e.g. 0, 1,2,3,2,1,0); will be clamped between 0.0 and 1.0</param>
+    /// <param name="invert">if true, the final index map will be inverted</param>
+    /// <returns></returns>
+    public int[] GetIndexMapTemp(
+        int availableViews,
+        int contentViews,
+        float yoyoStart,
+        bool invert,
+        bool invertIndices = false
+    )
+    {
+        int[] mapArray = new int[availableViews];
+        generate_indexmap(
+            ref mapArray,
+            availableViews,
+            contentViews,
+            0,
+            yoyoStart,
+            invert,
+            invertIndices
+        );
+        return mapArray;
     }
 }
