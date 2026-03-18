aaae4838aa7674e8e9942e5729cc07eb19340e7a
Phillip Hohenester <hohenester@ux3d.io>
2026-03-18 15:48:08 +0100

    fix

--- Scripts/RenderPipelines/HDRP/CustomPass.cs
+++ Scripts/RenderPipelines/HDRP/CustomPass.cs
@@ -12,7 +12,7 @@ namespace G3D.RenderPipeline.HDRP
         protected override void Execute(CustomPassContext ctx)
         {
             var camera = ctx.hdCamera.camera;
-            if (shouldPerformBlit(camera))
+            if (Helpers.isMainG3DCamera(camera))
             {
                 CoreUtils.SetRenderTarget(ctx.cmd, ctx.cameraColorBuffer, ClearFlag.None);
                 CoreUtils.DrawFullScreen(
@@ -22,35 +22,6 @@ namespace G3D.RenderPipeline.HDRP
                     shaderPassId: 0
                 );
             }
-        }
-
-        /// <summary>
-        /// Checks whether the camera is a G3D camera or a Mosaic Multiview camera and if the blit material has been set.
-        /// If so returns true, otherwise returns false.
-        /// </summary>
-        /// <param name="camera"></param>
-        /// <returns></returns>
-        static bool shouldPerformBlit(Camera camera)
-        {
-            if (camera.cameraType != CameraType.Game)
-                return false;
-            bool isG3DCamera = camera.gameObject.TryGetComponent<G3DCamera>(out var g3dCamera);
-            bool isG3DCameraEnabled = isG3DCamera && g3dCamera.enabled; // only do something if our component is enabled
-
-            bool isMosaicMultiviewCamera =
-                camera.gameObject.TryGetComponent<G3DCameraMosaicMultiview>(out var mosaicCamera);
-            bool isMosaicMultiviewCameraEnabled = isMosaicMultiviewCamera && mosaicCamera.enabled; // same check if it is a mosaic camera
-
-            if (!isG3DCameraEnabled && !isMosaicMultiviewCameraEnabled)
-                return false;
-
-            // skip all cameras created by the G3D Camera script
-            if (camera.name.StartsWith(G3DCamera.CAMERA_NAME_PREFIX))
-            {
-                return false;
-            }
-
-            return true;
         }
 
         protected override void Cleanup() { }
